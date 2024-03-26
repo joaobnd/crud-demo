32c764e9a17e0de76ab67d50fe42d86cb5ff973a
João Antônio Bandeira de Oliveira Martins <joao.martins@ccc.ufcg.edu.br>
2024-03-25 23:50:43 -0300

    remove modules to use standalone components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,7 @@ import { FeaturesComponent } from './components/features/features.component'
 export const routes: Routes = [
   {
     path: '',
-    component: FeaturesComponent,
-    loadChildren: () =>
-      import('./components/features/features.module').then(
-        (m) => m.FeaturesModule
-      ),
+    component: FeaturesComponent
   },
   { path: '**', redirectTo: 'auth' }
 ];
